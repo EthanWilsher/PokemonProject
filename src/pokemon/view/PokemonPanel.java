aa13aa878e04047f78b7e135eebd9c2943525d95
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-12-18 09:39:16 -0700

    added code constructors and imports into the panel

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,6 +1,44 @@
 package pokemon.view;
 
-public class PokemonPanel
+import pokemon.controller.PokemonController;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+
+public class PokemonPanel extends JPanel
 {
+	private PokemonController appController;
+	
+	public PokemonPanel(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
 
 }
